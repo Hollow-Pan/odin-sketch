ef675127738bde52125b039538e902a7f6a9f84b
Hollow-Pan <pradhumnbhati7@gmail.com>
2025-06-06 13:09:19 +0530

    basic stuff done; extra credits left

--- javascript.js
+++ javascript.js
@@ -1,31 +1,55 @@
 const container = document.querySelector(".container");
 
-const gridElement = [];
-const gridContainer = [];
-
 function makeGridEle(parent){
     let div = document.createElement("div");
     div.classList.add("grid-element");
     parent.appendChild(div);
-
-    // gridElement.push(div);
 }
-
-function makeGridRow(columns){
+function makeGridRow(side){
     let gContainer = document.createElement("div");
     gContainer.classList.add("grid-container");
     container.appendChild(gContainer);
-    gridContainer.push(gContainer);
 
-    for (let i = 0; i < columns; i++){
+    for (let i = 0; i < side; i++){
         makeGridEle(gContainer);
     }
 }
-
-function makeGrid(rows, columns){
-    for (let i = 0; i < rows; i++){
-        makeGridRow(columns);
+function makeGrid(side){
+    for (let i = 0; i < side; i++){
+        makeGridRow(side);
     }
 }
 
-makeGrid(16,16);
+function sketch(e){
+    e.target.style.backgroundColor = "pink";
+}
+function attachListeners(){
+    const gridElement = Array.from(document.querySelectorAll(".grid-element"));
+    gridElement.forEach((ele) => ele.addEventListener('mouseenter', sketch));
+}
+
+makeGrid(16);
+attachListeners();
+
+
+function clearGrid(){
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+function resetGrid(side){
+    clearGrid();
+    makeGrid(side);
+    attachListeners();
+}
+
+
+function promptGridSize(){
+    let ans = +prompt("Enter new grid size:");
+    while (isNaN(ans) || ans > 100 || ans < 1){
+        ans = +prompt("Enter new grid size (1-100):");
+    }
+    resetGrid(ans);
+}
+const button = document.querySelector(".grid-size");
+button.addEventListener('click', promptGridSize);
